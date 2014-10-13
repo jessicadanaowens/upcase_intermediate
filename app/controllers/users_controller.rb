1423306217515f6ccd13e221bb5bc9e5798c272f
Jessica Owens <jessicadanaowens@gmail.com>
2014-10-13 10:37:02 -0600

    User can create a shout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   respond_to :html
 
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
   def new
     @user = User.new
   end
---
+++ db/migrate/20141013153039_create_shout.rb
@@ -0,0 +1,11 @@
+class CreateShout < ActiveRecord::Migration
+  def change
+    create_table :shouts do |t|
+      t.string :body
+      t.belongs_to :user
+
+      t.timestamps
+    end
+    add_index :shouts, :user_id
+  end
+end
