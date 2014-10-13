47acb6ec06b418a8b03af64de7edc744ed16480b
Jessica Owens <jessicadanaowens@gmail.com>
2014-10-13 09:18:01 -0600

    user can register, sign_in and sign_out with monban gem

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.12'
+gem 'sqlite3'
+gem 'strong_parameters', '0.2.0'
+gem 'monban', '0.0.6'
+
+group :assets do
+  #clump gems together that represent some sort of functionality
+  #you can install gems without a particular group
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'turn', '~> 0.8.3', :require => false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '20f23d23f534031de6ebf3c64133430d937c676c024aea0aca1c59b12db7066fa2719013d2485aaff90e4f166f55dbf86334e4c0bb09141a3e412d31ee6a66e0'
